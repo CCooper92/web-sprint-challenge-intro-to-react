b6926d3b30dc4c21e6f1156cc20080886fb7ceaa
CCooper92 <am10ac17@gmail.com>
2021-01-22 16:14:36 -0800

    info on page, no styling

--- src/components/Character.js
+++ src/components/Character.js
@@ -6,14 +6,63 @@ import React from 'react';
 
 const Character = (props) => {
     const {data} = props;
+
+    const char = (
+        data.map(obj => {
+            return obj.name;
+        })
+    );
+    const gender = (
+        data.map(info => {
+            return info.gender;
+        })
+    );
+    const skin_color = (
+        data.map(info => {
+            return info.skin_color;
+        })
+    );
+    const height = (
+        data.map(info => {
+            return info.height;
+        })
+    );
+
+    const mass = (
+        data.map(info => {
+            return info.mass;
+        })
+    );
+    
+    
+
     return (
+      <div>  
         <div>
-            <h1>{data.name}</h1>;
-            <p>{data.gender}</p>
-            <p>{data.hair_color}</p>
-            <p>{data.height}</p>
-            <p>{data.mass}</p>
+            <h1>{char[0]}</h1>
+            <p>{gender[0]}</p>
+            <p>{skin_color[0]}</p>
+            <p>{height[0]}</p>
+            <p>{mass[0]}</p>
         </div>
+
+        <div>
+            <h1>{char[1]}</h1>
+            <p>{gender[1]}</p>
+            <p>{skin_color[1]}</p>
+            <p>{height[1]}</p>
+            <p>{mass[1]}</p>
+        </div>
+        
+        <div>
+            <h1>{char[2]}</h1>
+            <p>{gender[2]}</p>
+            <p>{skin_color[2]}</p>
+            <p>{height[2]}</p>
+            <p>{mass[2]}</p>
+        </div>
+
+      </div>  
 
     );
 };
